7dba4dc3b983a95da3e362bae5fb19469016061e
jpbadan <jpbadan@gmail.com>
2020-10-13 11:46:57 +0200

    Updates

---
+++ precache-manifest.38a57c3c6134159b6164db3415fef156.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a7c0760fc1dbfd7e5706aec58c7398",
+    "url": "/4_primeiro-projeto-react/index.html"
+  },
+  {
+    "revision": "9afcec14b7e6bc0f2d8c",
+    "url": "/4_primeiro-projeto-react/static/js/2.2ddcc4ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/4_primeiro-projeto-react/static/js/2.2ddcc4ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ceed09d50c340022d7",
+    "url": "/4_primeiro-projeto-react/static/js/main.30f38e20.chunk.js"
+  },
+  {
+    "revision": "a5ec80b23ffba3516ee2",
+    "url": "/4_primeiro-projeto-react/static/js/runtime-main.414c9b0a.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/4_primeiro-projeto-react/static/media/github.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/4_primeiro-projeto-react/static/media/logo.04f6798f.svg"
+  }
+]);
