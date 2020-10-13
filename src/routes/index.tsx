bcc282d4cb8f13a748e332879714b046da50e445
jpbadan <jpbadan@gmail.com>
2020-10-13 12:40:02 +0200

    Added browser router component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   // Funcoes q fazem apenas um retorno podem usar parenteses e o return estará subentendido
-  <Switch>
-    {/* O Path (sem o exact) faz uma verificacao de inclusao do endereço especificado */}
-    <Route path="/" exact component={Dashboard} />
-    <Route path="/repository/:repository+" component={Repository} />
-  </Switch>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Switch>
+      {/* O Path (sem o exact) faz uma verificacao de inclusao do endereço especificado */}
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/repository/:repository+" component={Repository} />
+    </Switch>
+  </BrowserRouter>
 );
 
 // Em <Route path="/repository/:repository+" component={Repository} />
